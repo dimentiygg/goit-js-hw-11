e3f4d6c54a80782c39bfb396ea3fc5f906ee2be8
dimentiygg <sfx670@gmail.com>
2024-02-25 20:37:59 +0200

    refactoring

--- src/main.js
+++ src/main.js
@@ -1,14 +1,36 @@
 import { getDataApi } from './js/pixabay-api';
 import { createMarkup } from './js/render-functions';
 
+import SimpleLightbox from 'simplelightbox';
+import 'simplelightbox/dist/simple-lightbox.min.css';
+
+import iziToast from 'izitoast';
+import 'izitoast/dist/css/iziToast.min.css';
+
 const form = document.querySelector('.search-form');
 const loader = document.querySelector('.loader');
+
 form.addEventListener('submit', onSubmit);
 function onSubmit(event) {
   event.preventDefault();
+
   document.querySelector('.gallery').innerHTML = '';
   if (form.inputForm.value !== '') {
     loader.style.display = 'block';
+    getDataApi().then(({ hits }) => {
+      if (hits.length === 0) {
+        iziToast.error({
+          title: 'Error',
+          message:
+            'Sorry, there are no images matching your search query. Please try again!',
+        });
+      }
+      createMarkup(hits);
+    });
+  } else {
+    iziToast.error({
+      title: 'Error',
+      message: 'field cannot be empty',
+    });
   }
-  createMarkup(getDataApi());
 }
